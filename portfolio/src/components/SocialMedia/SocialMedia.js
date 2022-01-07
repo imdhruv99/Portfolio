e3ce89b01e4d164276d5f61b07add8820baf7995
imdhruv99 <dpdhruvprajapati@gmail.com>
2022-01-07 09:48:44 +0530

    [UPDATED] Social media icons

--- portfolio/src/components/SocialMedia/SocialMedia.js
+++ portfolio/src/components/SocialMedia/SocialMedia.js
@@ -7,44 +7,66 @@ import {
     faTwitter,
     faInstagram,
     faLinkedin,
-    faGithub
+    faGithub,
+    faMedium,
+    faDev
 } from "@fortawesome/free-brands-svg-icons";
 
 const SocialMedia = () => {
     return (
         <div className="social-container">
             <a
-            href="https://www.facebook.com/dhruv.prajapati.2899"
-            className="facebook social"
+            href="https://github.com/imdhruv99"
+            className="github social"
             >
-            <FontAwesomeIcon icon={faFacebook} size="3x" />
+            <FontAwesomeIcon icon={faGithub} size="3x" />
             </a>
-            <a href="https://twitter.com/imdhruv_28" className="twitter social">
-            <FontAwesomeIcon icon={faTwitter} size="3x" />
-            </a>
-            <a
-            href="https://www.instagram.com/imdhruv_28/"
-            className="instagram social"
-            >
-            <FontAwesomeIcon icon={faInstagram} size="3x" />
-            </a>
-            <a
-            href="mailto:dpdhruvprajapati@gmail.com"
-            className="googleplus social"
-            >
-            <FontAwesomeIcon icon={faGooglePlus} size="3x" />
-            </a>
+
             <a
             href="https://www.linkedin.com/in/imdhruv99/"
             className="linkedin social"
             >
             <FontAwesomeIcon icon={faLinkedin} size="3x" />
             </a>
+
             <a
-            href="https://github.com/imdhruv99"
-            className="github social"
+            href="https://www.facebook.com/dhruv.prajapati.2899"
+            className="facebook social"
             >
-            <FontAwesomeIcon icon={faGithub} size="3x" />
+            <FontAwesomeIcon icon={faFacebook} size="3x" />
+            </a>
+
+            <a href="https://twitter.com/imdhruv_28" 
+            className="twitter social">
+            <FontAwesomeIcon icon={faTwitter} size="3x" />
+            </a>
+
+            <a
+            href="https://www.instagram.com/imdhruv_28/"
+            className="instagram social"
+            >
+            <FontAwesomeIcon icon={faInstagram} size="3x" />
+            </a>
+
+            <a
+            href="mailto:dpdhruvprajapati@gmail.com"
+            className="googleplus social"
+            >
+            <FontAwesomeIcon icon={faGooglePlus} size="3x" />
+            </a>
+            
+            <a
+            href="https://imdhruv99.medium.com/"
+            className="medium social"
+            >
+            <FontAwesomeIcon icon={faMedium} size="3x" />
+            </a>
+
+            <a
+            href="https://dev.to/imdhruv99"
+            className="dev social"
+            >
+            <FontAwesomeIcon icon={faDev} size="3x" />
             </a>
         </div>
     );
