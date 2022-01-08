b450746fc25f1f066097d4591ee00b4dbc4ca4cb
imdhruv99 <dpdhruvprajapati@gmail.com>
2022-01-08 20:27:08 +0530

    [ADDED] Footer

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,12 +3,15 @@ import Navbar from "./components/Navbar/Navbar";
 import "./components/Navbar/Navbar.css";
 import Header from "./components/Header/Header";
 import  "./components/Header/Header.css";
+import Footer from "./components/Footer/Footer";
+import "./components/Footer/Footer.css"
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <Footer />
     </div>
   );
 };
---
+++ portfolio/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p className="footer-text">
+                Made with <span role="img">❤️</span> by Dhruv Prajapati
+            </p>
+        </div>
+    );
+}
+
+export default Footer;
