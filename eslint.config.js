ddc5d1df50a5d29369f72f03dc99fd37df60a4e9
imdhruv99 <dpdhruvprajapati@gmail.com>
2024-12-21 18:49:59 +0530

    [UPDATED] configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,32 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsconfigParser from '@typescript-eslint/parser';
+import pluginReact from 'eslint-plugin-react';
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+/** @type {import('eslint').Linter.Config} */
+export default {
+    overrides: [
+        {
+            files: ['**/*.{js,mjs,cjs,ts,tsx}'],
+            languageOptions: {
+                globals: globals.browser,
+                parser: tsconfigParser,
+                parserOptions: {
+                    ecmaVersion: 2020,
+                    sourceType: 'module',
+                    ecmaFeatures: { jsx: true },
+                    project: './tsconfig.json',
+                },
+            },
+            plugins: ['@typescript-eslint', 'react'],
+            rules: {
+                ...pluginJs.configs.recommended.rules,
+                ...tseslint.configs.recommended.rules,
+                ...pluginReact.configs.recommended.rules,
+                'react/react-in-jsx-scope': 'off',
+                'no-console': 'warn',
+            },
+        },
+    ],
+};
