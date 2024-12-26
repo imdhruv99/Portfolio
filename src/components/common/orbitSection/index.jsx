93c561d33a35cfacf41a73b18ef7a02314beff77
imdhruv99 <dpdhruvprajapati@gmail.com>
2024-12-26 20:09:05 +0530

    [ADDED] Grid Layout

--- src/components/common/orbitSection/index.jsx
+++ src/components/common/orbitSection/index.jsx
@@ -6,9 +6,17 @@ import PropTypes from 'prop-types';
 const OrbitSection = ({ isDarkTheme }) => {
     return (
         <section className="orbitSection">
-            <div className="content">
-                <h1 className="title">DHRUV PRAJAPATI</h1>
-                <p className="subtitle">Senior Software Engineer</p>
+            <div className='grid-container'>
+                <div className='content-left'>
+
+                </div>
+                <div className="content-right">
+                    <h1 className="title">DHRUV PRAJAPATI</h1>
+                    <p className="subtitle">Software Connoisseur</p>
+                    <p className="description">
+                        I craft intelligent, data-driven experiences - blending full-stack expertise with AI innovation.
+                    </p>
+                </div>
             </div>
             <OrbitScene isDarkTheme={isDarkTheme} />
         </section>
