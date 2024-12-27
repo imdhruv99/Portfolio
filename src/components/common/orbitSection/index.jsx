fa24aa121bc0b8da1d0d68c4924d4be9c382cea2
imdhruv99 <dpdhruvprajapati@gmail.com>
2024-12-27 22:22:19 +0530

    [UPDATED] image

--- src/components/common/orbitSection/index.jsx
+++ src/components/common/orbitSection/index.jsx
@@ -3,16 +3,18 @@ import './orbitSection.css';
 import OrbitScene from './orbitScene.jsx';
 import PropTypes from 'prop-types';
 
+import profileImage from '../../../assets/images/profile.jpg'
+
 const OrbitSection = ({ isDarkTheme }) => {
     return (
         <section className="orbitSection">
             <div className='grid-container'>
                 <div className='content-left'>
-
+                    <img src={profileImage} alt="Profile" className="left-image" />
                 </div>
                 <div className="content-right">
                     <h1 className="title">DHRUV PRAJAPATI</h1>
-                    <p className="subtitle">Software Connoisseur</p>
+                    <p className="subtitle">Software Engineer</p>
                     <p className="description">
                         I craft intelligent, data-driven experiences - blending full-stack expertise with AI innovation.
                     </p>
